ab9ca6eb8285dec786e47aba72d4dcd756f17f3e
Nikhil R <rnikhil2751996@gmail.com>
2015-02-25 16:34:59 +0530

    Added Authentication

--- Slim/index.php
+++ Slim/index.php
@@ -11,32 +11,54 @@ R::setup('mysql:host=localhost;dbname=appdata','root','');
 R::freeze(true); //prevent anymore changes to repo from redbeans
 
 // initialize app
+<?php
+// do initial application and database setup
+
+// initialize app
 $app = new \Slim\Slim();
 
-class ResourceNotFoundException extends Exception {}
+// route middleware for simple API authentication
+function authenticate(\Slim\Route $route) {
+  $app = \Slim\Slim::getInstance();
+  $uid = $app->getEncryptedCookie('uid');
+  $key = $app->getEncryptedCookie('key');
+  if (validateUserKey($uid, $key) === false) {
+    $app->halt(401);
+  }
+}
 
-  // handle GET requests for /articles/:id
-  $app->get('/articles/:id', function ($id) use ($app) {    
-    try {
-      // query database for single article
-      $article = R::findOne('articles', 'id=?', array($id));
+function validateUserKey($uid, $key) {
+  // insert your (hopefully more complex) validation routine here
+  if ($uid == 'demo' && $key == 'demo') {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-      if ($article) {
-        // if found, return JSON response
-        $app->response()->header('Content-Type', 'application/json');
-        echo json_encode(R::exportAll($article));
-      } else {
-        // else throw exception
-        throw new ResourceNotFoundException();
-      }
-    } catch (ResourceNotFoundException $e) {
-      // return 404 server error
-      $app->response()->status(404);
-    } catch (Exception $e) {
-      $app->response()->status(400);
-      $app->response()->header('X-Status-Reason', $e->getMessage());
-    }
-  });
+// handle GET requests for /articles
+$app->get('/articles', 'authenticate', function () use ($app) {
+  // query database for all articles
+  $articles = R::find('articles');
 
-  // run
-  $app->run();
+  // send response header for JSON content type
+  $app->response()->header('Content-Type', 'application/json');
+
+  // return JSON-encoded response body with query results
+  echo json_encode(R::exportAll($articles));
+});
+
+// generates a temporary API key using cookies
+// call this first to gain access to protected API methods
+$app->get('/demo', function () use ($app) {
+  try {
+    $app->setEncryptedCookie('uid', 'demo', '5 minutes');
+    $app->setEncryptedCookie('key', 'demo', '5 minutes');
+  } catch (Exception $e) {
+    $app->response()->status(400);
+    $app->response()->header('X-Status-Reason', $e->getMessage());
+  }
+});
+
+// run
+$app->run();
